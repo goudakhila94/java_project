e219b8cc81565d3378c6714375938de393da1153
goudakhila94 <goudakhila94@gmail.com>
2026-03-31 11:29:45 +0530

    Create Restaurant.java

---
+++ Restaurant.java
@@ -0,0 +1,13 @@
+package review_1;
+
+class Restaurant
+{
+    String name;
+    double rating;
+
+    Restaurant(String name, double rating)
+    {
+        this.name = name;
+        this.rating = rating;
+    }
+}
