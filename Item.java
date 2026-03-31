0699de35c8b72a814cab1179a40441059ef616c5
goudakhila94 <goudakhila94@gmail.com>
2026-03-31 11:33:33 +0530

    Create Item.java

---
+++ Item.java
@@ -0,0 +1,15 @@
+package review_1;
+
+public class Item
+{
+    String name;
+    int price;
+    int quantity;
+
+    Item(String name, int price, int quantity)
+    {
+        this.name = name;
+        this.price = price;
+        this.quantity = quantity;
+    }
+}
